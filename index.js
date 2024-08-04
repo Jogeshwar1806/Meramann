16e775751f20846710b0fd5f47f92109296a1051
Jogeshwar1806 <jogeshwarmishra2003@gmail.com>
2024-08-05 04:38:57 +0530

    Thoda sa change kiye hai

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 const methodOverride = require("method-override");
 const { faker } = require("@faker-js/faker");
 const { encode } = require("querystring");
@@ -15,16 +15,6 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   database: "Meramann",
-//   password: "200306",
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -43,68 +33,59 @@ app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
+//sign-in in website
+app.post("/signin",(req,res)=>{
+  let {email,password} = req.body;
+  let q = `SELECT password from posts where email="${email}";`;
+  try {
+
+    connection.query(q,(err,result)=>{
+      let pass = result[0].password;
+      if(pass==password){
+        res.redirect("/posts");
+      }
+      if(err) throw err;
+    });
+  } catch (err) {
+    console.log(err);
+  }
+})
+
 //sign-up in database
-// app.post("/signup", async (req, res) => {
-//   let q = "select * from posts";
-//   let connection;
-//   try {
-//     connection = await pool.getConnection();
-//     const [result] = await connection.query(q);
-//     console.log(result);
-//     res.send("result");
-//     // throw err;
-//   } catch (err) {
-//     console.log(err);
-//   } finally {
-//     if (connection) {
-//       console.log("Releasing connection back to the pool...");
-//       connection.release();
-//     }
-//   }
-// });
-// app.post("/signup", async (req, res) => {
-//   let q = "select * from posts";
-//   try {
-//     const [result] = connection.query(q);
-//     console.log(result);
-//     res.send("result");
-//   } catch (err) {
-//     console.log(err);
-//   } finally {
-//     if (connection) {
-//       console.log("Releasing connection back to the pool...");
-//       connection.release();
-//     }
-//   }
-// });
-async function createConnection() {
-    try {
-        const connection = await mysql.createConnection(dbConfig);
-        console.log('Connected to the database');
-        return connection;
-    } catch (error) {
-        console.error('Error connecting to the database:', error.message);
-        throw error;
-    }
-}
-app.post("/signup", async (req, res) => {
-    const query = "SELECT * FROM posts";
-    let connection;
-    try {
-        connection = await createConnection();
-        const [result] = await connection.execute(query);
-        console.log(result);
-        res.send(result); // Send the result back
-    } catch (err) {
-        console.error('Error during database operation:', err.message);
-        res.status(500).send('Server Error');
-    } finally {
-        if (connection) {
-            await connection.end();
-            console.log('Connection closed');
-        }
-    }
+app.post("/signup", (req, res) => {
+  let {username,email,password} = req.body;
+  console.log(faker.string.uuid(),username,email,password);
+  let q = `Insert into posts (id,username,email,password) values ("${faker.string.uuid()}","${username}","${email}","${password}");`;
+  try {
+    connection.query(q,(err,result)=>{
+      console.log(result);
+      if(err) throw err;
+      res.redirect("/posts");
+    })
+  } catch (err) {
+    console.log(err);
+  }
 });
+
+//show all posts
+app.get("/posts", (req, res) => {
+  let q = "SELECT * FROM posts;";
+  try {
+    connection.query(q, (err, result) => {
+      console.log(result);
+      console.log(faker.string.uuid());
+      res.render("posts.ejs",{result});
+      if(err) throw err;
+    });
+  } catch (err){
+    console.log(err);
+  }
+});
+
+//forget password page
+app.get("/forget",(req,res)=>{
+  res.render("forget.ejs");
+})
 
 //search page
 app.get("/search", (req, res) => {
